05d4d40e1e6bbb5bc0d6112b0a0b516a099ba378
ASUS <abhiraskar@gmail.com>
2023-07-28 13:28:58 +0530

    frontend

---
+++ frontend/src/Components/HomePage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <select name="" id="">
+        <option value="">Select Course</option>
+        <option value="MERN">MERN</option>
+        <option value="NODE">NODE</option>
+        <option value="JAVA">JAVA</option>
+        <option value="OTHERS">OTHERS</option>
+      </select>
+    </div>
+  );
+};
+
+export default HomePage;
