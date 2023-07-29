a908624ba26165930f8d043737326e72ac130f23
ASUS <abhiraskar@gmail.com>
2023-07-29 12:37:49 +0530

    homepage& navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import HomePage from './Components/HomePage';
 import Interview from './Components/Interview';
+import Navbar from './Components/Navbar';
 import ChatInterface from './Components/test';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
-      {/* <HomePage /> */}
+      <Navbar />
+      <HomePage />
       <Interview category={"NodeJS"} />
       {/* {<ChatInterface />} */}
     </div>
