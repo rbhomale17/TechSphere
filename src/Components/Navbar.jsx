185283e5389bcf25d10774ebcee6d23592462800
rbhomale17 <rushikeshbhomale@gmail.com>
2023-08-10 15:00:02 +0530

    Logo changed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <div className="fixed top-0 left-0 w-full bg-blue-200 text-white shadow-md p-2 flex justify-center items-center z-10">
       <img
-        src="https://techsphere.app/static/media/primarylogo-color.da88ef7d.png"
+        src="https://petbuddy-main-server.onrender.com/photos/files/64d4ac76637c63b0bbb08ae8"
         width={"20%"}
         alt=""
         onClick={() => navigate("/")}
